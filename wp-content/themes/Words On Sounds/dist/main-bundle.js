884b87c70aec037c66b67a0879b2343a99003b98
Adam Riggs <phlux1111@gmail.com>
2021-01-15 13:39:57 -0500

    search page and new fonts

--- wp-content/themes/Words On Sounds/dist/main-bundle.js
+++ wp-content/themes/Words On Sounds/dist/main-bundle.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5881ac3ec8c556d524bf";
+/******/ 	var hotCurrentHash = "9ea9b86c553b30f6c82c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
